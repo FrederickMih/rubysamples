78fca823e61fee315535763afea8d91ed400cc8f
FrederickMih <fredtimermih@gmail.com>
2022-07-07 00:32:39 +0100

    Requiring a feature not a file

--- loadFilesAndExtensions/loaddemo.rb
+++ loadFilesAndExtensions/loaddemo.rb
@@ -1,3 +1,4 @@
 puts "This is the first (master) program file."
-load "loadee.rb"
+#load "loadee.rb" # We load a file not a feature.
+require "./loadee" # We don't require a file, we require a feature
 puts "And back again to the first file."
